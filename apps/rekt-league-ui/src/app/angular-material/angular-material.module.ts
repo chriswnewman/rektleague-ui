5cdc4fa595dc93a05132dcadb35de70c919a7d48
Chris Newman <cwnewman1992@gmail.com>
2019-06-20 16:42:16 -0400

    mat menu instead of select

--- apps/rekt-league-ui/src/app/angular-material/angular-material.module.ts
+++ apps/rekt-league-ui/src/app/angular-material/angular-material.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSelectModule} from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatToolbarModule, MatButtonModule, MatSelectModule, MatFormFieldModule]
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatIconModule
+  ]
 })
 export class AngularMaterialModule {}
